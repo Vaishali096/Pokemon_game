5cf5da0bc8e434ddcf620a13374583f6c566e721
hari-pace <hari.pace23@gmail.com>
2023-10-11 13:46:40 +0200

    dark-mode added

--- src/Components/PokemonList.jsx
+++ src/Components/PokemonList.jsx
@@ -1,19 +1,53 @@
 import { Link } from "react-router-dom";
-import './PokemonList.css';
-export default function PokemonList({pokemons}) {
-    return(
-      <>
-    <div className="PokemonList">
+import "./PokemonList.css";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
+export default function PokemonList({ pokemons }) {
+  const { light, dark, isLightTheme, toggleTheme } = useContext(ThemeContext);
+
+  const themeStyles = isLightTheme ? light : dark;
+
+  return (
+    <>
+      <div
+        className="PokemonList"
+        style={{ background: themeStyles.ui, color: themeStyles.text }}
+      >
         <h1>PokeFight</h1>
-    { pokemons
-        ? pokemons.map((pokemon) => (
-            <div className="Pokemon" key={pokemon.id}>
-              <Link to={`/${pokemon.name.english}`}><h4>{pokemon.name.english}</h4></Link>
-              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} />
-            </div>
-          ))
-        : "No pokemons to display"}
-        </div>
-        </>
-    );
+        <button
+          style={{ background: themeStyles.ui, color: themeStyles.text }}
+          onClick={toggleTheme}
+        >
+          {isLightTheme ? "🌙" : "💡"}{" "}
+        </button>
+        {pokemons
+          ? pokemons.map((pokemon) => (
+              <div
+                style={{
+                  background: themeStyles.bg,
+                  color: themeStyles.text,
+                }}
+                className="Pokemon"
+                key={pokemon.id}
+              >
+                <Link to={`/${pokemon.name.english}`}>
+                  <h4
+                    style={{
+                      background: themeStyles.bg,
+                      color: themeStyles.text,
+                    }}
+                  >
+                    {pokemon.name.english}
+                  </h4>
+                </Link>
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                />
+              </div>
+            ))
+          : "No pokemons to display"}
+      </div>
+    </>
+  );
 }
