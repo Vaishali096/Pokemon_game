b0c43804f8af3863dc8c3122e8873d66370b5383
hari-pace <hari.pace23@gmail.com>
2023-10-11 12:32:09 +0200

    fight page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
-import axios from 'axios';
+import axios from "axios";
 import Navbar from "./Components/Navbar";
-import PokemonInfo from './Components/PokemonInfo';
-import PokemonList from './Components/PokemonList';
+import PokemonInfo from "./Components/PokemonInfo";
+import PokemonList from "./Components/PokemonList";
+import Fight from "./Components/Fight";
 
 function App() {
-    const [pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState("");
 
-    const getPokemon = async () => {
-      const res = await axios.get("https://pokefight-backend-cbka.onrender.com/pokemon");
-      console.log(res);
-      setPokemons(res.data);
-    };
+  const getPokemon = async () => {
+    const res = await axios.get(
+      "https://pokefight-backend-cbka.onrender.com/pokemon"
+    );
+    console.log(res);
+    setPokemons(res.data);
+  };
 
-    useEffect(()=>{
-      getPokemon();
-    },[]);
-  
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
   return (
     <>
-
-    <Routes>
-      <Route path="/" element={<PokemonList pokemons={pokemons}/>}/>
-      <Route path="/:name" element={<PokemonInfo pokemons={pokemons}/>}/>
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<PokemonList pokemons={pokemons} />} />
+        <Route
+          path="/:name"
+          element={
+            <PokemonInfo
+              pokemons={pokemons}
+              setSelectedPokemon={setSelectedPokemon}
+            />
+          }
+        />
+        <Route
+          path="/fight"
+          element={<Fight selectedPokemon={selectedPokemon} />}
+        />
+      </Routes>
     </>
   );
 }
--- src/Components/PokemonInfo.jsx
+++ src/Components/PokemonInfo.jsx
@@ -1,29 +1,43 @@
 import { useParams, useNavigate } from "react-router-dom";
-import './PokemonInfo.css';
+import "./PokemonInfo.css";
+import { Link } from "react-router-dom";
 
-export default function PokemonInfo({ pokemons}) {
-    const navigate = useNavigate();
-    const { name } = useParams();
+export default function PokemonInfo({ pokemons, setSelectedPokemon }) {
+  const navigate = useNavigate();
+  const { name } = useParams();
 
-    const onePokemon = pokemons && pokemons.find((pokemon) => pokemon.name.english == (name));
+  const onePokemon =
+    pokemons && pokemons.find((pokemon) => pokemon.name.english == name);
 
-    return (
-        <div className="PokemonDetails">
-
-        <h3>Name</h3>
-        {onePokemon && <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${onePokemon.id}.png`} />}
-        <p>{onePokemon && onePokemon.name.english}</p>
-        <p>{onePokemon && onePokemon.name.japanese}</p>
-        <p>{onePokemon && onePokemon.name.chinese}</p>
-        <p>{onePokemon && onePokemon.name.french}</p>
-        <h3>Type</h3>
-        <p>{onePokemon && onePokemon.type}</p>
-        <h3>Base</h3>
-        <p>HP: {onePokemon && onePokemon.base.HP}</p>
-        <p>Attack: {onePokemon && onePokemon.base.Attack}</p>
-        <p>Defense: {onePokemon && onePokemon.base.Defense}</p>
-        <p>Speed: {onePokemon && onePokemon.base.Speed}</p>
-        <p>V: {onePokemon && onePokemon.__v}</p>
-        </div>
-    );
+  return (
+    <div className="PokemonDetails">
+      <h3>Name</h3>
+      {onePokemon && (
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${onePokemon.id}.png`}
+        />
+      )}
+      <p>{onePokemon && onePokemon.name.english}</p>
+      <p>{onePokemon && onePokemon.name.japanese}</p>
+      <p>{onePokemon && onePokemon.name.chinese}</p>
+      <p>{onePokemon && onePokemon.name.french}</p>
+      <h3>Type</h3>
+      <p>{onePokemon && onePokemon.type}</p>
+      <h3>Base</h3>
+      <p>HP: {onePokemon && onePokemon.base.HP}</p>
+      <p>Attack: {onePokemon && onePokemon.base.Attack}</p>
+      <p>Defense: {onePokemon && onePokemon.base.Defense}</p>
+      <p>Speed: {onePokemon && onePokemon.base.Speed}</p>
+      <Link to="/fight">
+        <button
+          className="Pokemon-info-select-pokemon"
+          onClick={() => {
+            setSelectedPokemon(onePokemon.id);
+          }}
+        >
+          Select this Pokemon and proceed to fight page!
+        </button>
+      </Link>
+    </div>
+  );
 }
