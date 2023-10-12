4dcbec0405ce823b71fba8b4680a547c6091afb9
Vaishali096 <vaishalivenkateshwaran@gmail.com>
2023-10-12 11:49:30 +0200

    Pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
-import ReactPaginate from 'react-paginate';
+// import ReactPaginate from 'react-paginate';
 import axios from 'axios';
 import Navbar from "./Components/Navbar";
 import PokemonInfo from './Components/PokemonInfo';
 import PokemonList from './Components/PokemonList';
-// import Pagination from './Components/Pagination';
+import Pagination from './Components/Pagination';
 
 function App() {
     const [pokemons, setPokemons] = useState([]);
-    const[currentPage, setCurrentPage] = useState(0);
-    const[totalPages, setTotalPages] = useState(0);
-
-    // const[postsPerPage, setPostsPerPage]= useState(0);
-    // const[arrOfCurrentButtons, setArrOfCurrentButtons] = useState([]);
+    const[currentPage, setCurrentPage] = useState(1);
+    const[postsPerPage, setPostsPerPage]= useState(9);
 
 
     const getPokemon = async () => {
-      const res = await axios.get("https://pokefight-backend-cbka.onrender.com/pokemon",
-      {
-        params:{page: currentPage},
-      });
+      const res = await axios.get("https://pokefight-backend-cbka.onrender.com/pokemon");
       console.log(res);
       setPokemons(res.data);
     };
@@ -30,40 +24,20 @@ function App() {
       getPokemon();
     },[currentPage]);
 
-    // const lastPostIndex = currentPage * postsPerPage;
-    // const firstPostIndex = lastPostIndex - postsPerPage;
-    // const currentPosts = pokemons.slice(firstPostIndex, lastPostIndex);
+    const lastPostIndex = currentPage * postsPerPage;
+    const firstPostIndex = lastPostIndex - postsPerPage;
+    const currentPosts = pokemons.slice(firstPostIndex, lastPostIndex);
 
-    const handlePageChange = event =>{
-      console.log(event);
-      setCurrentPage(event.selected);
-    }
 
   return (
     <>
     <Routes>
-    <Route path="/" element={<PokemonList pokemons={pokemons}/>}/>
+    <Route path="/" element={<PokemonList pokemons={currentPosts}/>}/>
       <Route path="/:name" element={<PokemonInfo pokemons={pokemons}/>}/>
     </Routes>
-    {/* <div>
-    <Pagination totalPosts={pokemons.length} postsPerPage={postsPerPage} setCurrentPage={setCurrentPage}/>
-    </div> */}
-
-    <>
-<ReactPaginate
-        nextLabel=">>"
-        previousLabel="<<"
-        breakLabel="..."
-        forcePage={currentPage}
-        pageCount={totalPages}
-        renderOnZeroPageCount={null}
-        onPageChange={handlePageChange}
-        className="pagination"
-        activeClassName="active-page"
-        previousClassName="previous-page"
-        nextClassName="next-page"
-/>
-    </>
+    <div>
+    <Pagination totalPosts={pokemons.length} postsPerPage={postsPerPage} setCurrentPage={setCurrentPage} />
+    </div>
     </>
   );
 }
--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,77 +1,40 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-// const Pagination = ({ totalPosts, postsPerPage, setCurrentPage })=>{
-//     const[pageNoLimit, setPageNoLimit] = useState(15);
-//     const[maxPageNoLimit, setMaxPageNoLimit] = useState(8);
-//     const[minPageNoLimit, setMinPageNoLimit] = useState(1);
-//     const[arrOfCurrentButtons, setArrOfCurrentButtons] = useState([]);
-
-
-//     // const handleClick = (event)=>{
-//     //     setCurrentPage(Number(event.target.id));
-//     // };
-
-//     let pages = [];
-//     for(let i = 1; i<= Math.ceil(totalPosts/postsPerPage); i++){
-//         pages.push(i);
-//     }
-
-//     // const renderPageNumbers = pages.map((number)=>{
-//     //     if(number < maxPageNoLimit+1 && number>minPageNoLimit){
-//     //         return(
-//     //             <div className='pageNo' key={number}
-//     //             //  id={number}
-//     //             //  onClick={setCurrentPage == number? "active: null"}
-//     //             >
-//     //             {number}
-//     //             </div>
-//     //         );
-//     //     }else {
-//     //         return null;
-//     //     }
-//     // });
-
-    
-// useEffect(()=>{
-// let tempNoOfPages = [...pages];
-// tempNoOfPages = tempNoOfPages.slice(0,15);
-// setArrOfCurrentButtons(tempNoOfPages);
-//     handleNextBtn();
-//   },[setCurrentPage]);
-
-// const handleNextBtn = ()=>{
-//     setCurrentPage((prev)=> prev===1? prev: prev - 1);
-//     if(setCurrentPage+1> maxPageNoLimit){
-//         setMaxPageNoLimit(maxPageNoLimit+ postsPerPage);
-//         setMinPageNoLimit(minPageNoLimit+ postsPerPage);
-//       }
-//   };
-
-//     return (
-//         <>
-
-//         <div>
-//             <button onClick = {()=>setCurrentPage((prev)=> prev===1? prev: prev - 1)}>Prev</button>
-//             {arrOfCurrentButtons.map((page, index) => {
-//                     return <button key={index} onClick={()=>setCurrentPage(page)}>{page}</button>;
-//                 })}
-//             {/* <button onClick = {()=>setCurrentPage((prev)=> prev === pages.length? prev: prev + 1)}>Next</button> */}
-//             <button onClick={handleNextBtn}>Next</button>
-//                 </div>
-//         // <>
-//         // <button>Prev</button>
-
-//         // <li>{renderPageNumbers}</li>
-
-//         // <button>Next</button>
-//         // </>
-
-//     <>
-
-//     </>
+const Pagination = ({ totalPosts, postsPerPage, setCurrentPage })=>{
+    const[pageNoLimit, setPageNoLimit] = useState(0);
+    const[maxPageNoLimit, setMaxPageNoLimit] = useState(15);
+    const[minPageNoLimit, setMinPageNoLimit] = useState(1);
 
 
-//         </>
-//     );
-// };
-// export default Pagination;
+
+    let pages = [];
+    for(let i = 1; i<= Math.ceil(totalPosts/postsPerPage); i++){
+        pages.push(i);
+    }
+  
+useEffect(()=>{
+    handleNextBtn();
+  },[setCurrentPage]);
+
+const handleNextBtn = ()=>{
+    setCurrentPage((prev)=> prev===1? prev: prev + 1);
+    if(setCurrentPage+1> maxPageNoLimit){
+        setMaxPageNoLimit(maxPageNoLimit+ postsPerPage);
+        setMinPageNoLimit(minPageNoLimit+ postsPerPage);
+      }
+  };
+
+    return (
+        <>
+        <div>
+            <button onClick = {()=>setCurrentPage((prev)=> prev===1? prev: prev - 1)}>Prev</button>
+            {pages.map((page, index) => {
+                    return <button key={index} onClick={()=>setCurrentPage(page)}>{page}</button>;
+                })}
+            {/* <button onClick = {()=>setCurrentPage((prev)=> prev === pages.length? prev: prev + 1)}>Next</button> */}
+            <button onClick={handleNextBtn}>Next</button>
+                </div>
+        </>
+    );
+};
+export default Pagination;
