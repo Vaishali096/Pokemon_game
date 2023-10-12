d4afd4bc35baaaf5b7720dd1656408609f4c20bb
Vaishali096 <vaishalivenkateshwaran@gmail.com>
2023-10-12 22:50:06 +0200

    Pagination css

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     const [pokemons, setPokemons] = useState([]);
     const [selectedPokemon, setSelectedPokemon] = useState("");
     const[currentPage, setCurrentPage] = useState(1);
-    const[postsPerPage, setPostsPerPage]= useState(3);
+    const[postsPerPage, setPostsPerPage]= useState(12);
 
     const getPokemon = async () => {
       const res = await axios.get("https://pokefight-backend-cbka.onrender.com/pokemon",
