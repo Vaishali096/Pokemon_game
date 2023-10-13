38d38dc18af0fe398cadc9e1ece64e7da5257cda
Renata Siquieroli <resiquieroli@gmail.com>
2023-10-13 14:57:59 +0200

    pokemon per page

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const [pokemons, setPokemons] = useState([]);
   const [selectedPokemon, setSelectedPokemon] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(16);
+  const [postsPerPage, setPostsPerPage] = useState(20);
   const [newUsername, setNewUsername] = useState("");
 
   const getPokemon = async () => {
